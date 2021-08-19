c6cd054f6e3b0838292b81676bf6c80118e6c320
Josh Leubecher-Robinson <joshleubecherrobinson@gmail.com>
2021-08-19 13:02:12 -0400

    Refactored navbar and now includes real links

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,) { }
 
   ngOnInit(): void {
+  }
+
+  openProjects(): void {
+    this.router.navigate(['projects']);
+  }
+  openDevelopers(): void {
+    this.router.navigate(['developers']);
+  }
+  openHome(): void {
+    this.router.navigate(['/']);
   }
 
 }
