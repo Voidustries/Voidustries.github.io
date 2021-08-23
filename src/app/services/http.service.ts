a9053615428eef9f9d6804f0ff72d95a7eb2d65d
Josh Leubecher-Robinson <joshleubecherrobinson@gmail.com>
2021-08-23 10:02:00 -0400

    Add httpService for componants to make calls from

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Repositories, Repository } from '../models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  private void_projects_url = "https://raw.githubusercontent.com/Voidustries/Voidustries.github.io/master/projects/projects.json"
+
+  constructor(private http: HttpClient) { }
+
+  getProjects(): Observable<Repositories<Repository>> {
+    return this.http.get<Repositories<Repository>>(this.void_projects_url);
+  } 
+
+}
