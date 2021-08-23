d9149c130c9bed63baada2e03727710847630e2b
Josh Leubecher-Robinson <joshleubecherrobinson@gmail.com>
2021-08-23 17:27:25 -0400

    possible open graph metadata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.sass'],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public title = 'Voidustries';
+
+  constructor(
+    private metaTagService: Meta,
+    ) { }
+
+  ngOnInit(): void {
+    this.metaTagService.addTags([
+      { name: "og:title", content: "Voidustries | Home"},
+      { name: "og:description", content: "Voidustries home page."},
+      { name: "og:image", content: "https://www.voidustries.ca/android-chrome-512x512.png"},
+      { name: "og:url", content: "https://www.voidustries.ca"},
+    ]);
+  }
 }
