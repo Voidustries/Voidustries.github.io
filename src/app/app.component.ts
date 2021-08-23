20463c7ce31cbea682558a2bee412a5578970aa7
Josh Leubecher-Robinson <joshleubecherrobinson@gmail.com>
2021-08-23 18:09:25 -0400

    Add static meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { Meta } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['./app.component.sass'],
   templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   public title = 'Voidustries';
-
-  constructor(
-    private metaTagService: Meta,
-    ) { }
-
-  ngOnInit(): void {
-    this.metaTagService.addTags([
-      { name: "og:title", content: "Voidustries | Home"},
-      { name: "og:description", content: "Voidustries home page."},
-      { name: "og:image", content: "https://www.voidustries.ca/android-chrome-512x512.png"},
-      { name: "og:url", content: "https://www.voidustries.ca"},
-    ]);
-  }
 }
